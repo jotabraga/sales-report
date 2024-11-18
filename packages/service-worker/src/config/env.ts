4d19fc894152dfb0b3db7fdb2906e4ac70918855
jotabraga <jpmbraga.feg@gmail.com>
2024-11-18 11:52:52 -0300

    fix: create log helper

--- packages/service-worker/src/config/env.ts
+++ packages/service-worker/src/config/env.ts
@@ -4,16 +4,10 @@ dotenv.config();
 export const config = {
   port: Number(process.env.PORT) || 3001,
   rabbitMqUrl: process.env.RABBITMQ_URL || "",
-  customersApiBaseUrl: process.env.CUSTOMERS_API_BASE_URL || "",
-  productsApiBaseUrl: process.env.PRODUCTS_API_BASE_URL || "",
-  salesApiBaseUrl: process.env.SALES_API_BASE_URL || "",
+  apiBaseUrl: process.env.API_BASE_URL || "",
 };
 
-const isMissingMandatoryVars =
-  !config.rabbitMqUrl ||
-  !config.customersApiBaseUrl ||
-  !config.productsApiBaseUrl ||
-  !config.salesApiBaseUrl;
+const isMissingMandatoryVars = !config.rabbitMqUrl || !config.apiBaseUrl;
 
 if (isMissingMandatoryVars) {
   throw new Error("Missing required environment variables");
